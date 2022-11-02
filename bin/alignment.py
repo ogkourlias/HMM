192504469fa8ee6dac923c15964ccdf6cf4cd9d6
ogkourlias <orfeasgkourlias@gmail.com>
2022-11-02 11:56:04 +0100

    Wrote more

--- bin/alignment.py
+++ bin/alignment.py
@@ -11,7 +11,6 @@ __status__ = "WIP"
 __version__ = "0.1"
 
 # IMPORTS
-import sys
 from Bio import AlignIO
 
 
@@ -24,17 +23,4 @@ class alignment:
         self.align = AlignIO.read(file_path, "fasta")
 
     def __str__(self):
-        return self.align
-
-
-# FUNCTIONS
-def y():
-    """ Desc """
-    return 0
-
-
-# MAIN
-def main(args):
-    """ Main function """
-    # FINISH
-    return 0
+        return self.align.__str__()
---
+++ bin/hmmer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+    usage:
+        python3 main.py
+"""
+
+# METADATA VARIABLES
+__author__ = "Orfeas Gkourlias"
+__status__ = "WIP"
+__version__ = "0.1"
+
+# IMPORT
+import subprocess
+
+# CLASSES
+class hmmer:
+    """ Desc """
+
+    def hmmer_san(self):
+        os.system("hmmscan")
